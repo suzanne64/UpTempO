db954f33bfb1ca254072775289f43c4447d118b1
suzanne64 <sdickins@uw.edu>
2022-02-16 16:06:10 -0800

    added nsidc downloads code (modified from NSIDC code)

--- UPDATE_UPTEMPO.py
+++ UPDATE_UPTEMPO.py
@@ -10,6 +10,7 @@ import UpTempO_Python as upy
 import UpTempO_Downloads as upd
 import UpTempO_ProcessRaw as upp
 import UpTempO_Plots as uplots
+import ArcticPlots as aplots
 
 def upupData():
     
@@ -19,7 +20,7 @@ def upupData():
 
     #---- Update Data ----
     for r in reporting:  # reporting is a dict, keys are buoy ID
-        # if '300534062158460' in r:
+        # if '300534060251600' in r:
 
         source,user=reporting[r][3].split(':')
         if source == 'PG':
@@ -27,7 +28,6 @@ def upupData():
             # upd.getPG(r,user)
             upd.getPG(r,user,reporting[r][2])
             upp.processPG(r)
-            # exit(-1)
             datpath, latestupdate = upp.WebFormat(r)
         if source == 'ARGOS':
             print('getting '+r+' from ARGOS:'+user)
@@ -37,12 +37,12 @@ def upupData():
             
         fname=datpath.split('/')[-1]
         opdat.write(datpath+' WebDATA/'+fname+'\n')
-
-    if newdead:
-        for nd in newdead:
-            source=newdead[nd][-1].split(':')[0]
-            if source == 'ARGOS': datpath=upp.WebFormat(nd,order=1,fts=0,newdead=1)
-            else: datpath=upp.WebFormat(nd,newdead=1)
+    
+        if newdead:
+            for nd in newdead:
+                source=newdead[nd][-1].split(':')[0]
+                if source == 'ARGOS': datpath=upp.WebFormat(nd,order=1,fts=0,newdead=1)
+                else: datpath=upp.WebFormat(nd,newdead=1)
             
 
     opdat.close()
@@ -51,18 +51,19 @@ def upupData():
             
             
 def upupPlots():
-    # nopresbids=['300534062158460','300534062158480']
-    # nosalibids=['300534060051570','300534060251600','300534060649670','300234068519450']
-    # curbuoys,deadbuoys,orderbuoys,newdead=BM.getBuoys()
-    # for c in curbuoys:
-    #     uplots.TimeSeriesPlots(bid=c)
-    #     if c not in nopresbids:
-    #         uplots.TimeSeriesPlots(bid=c,quan='Pressure')
-    #     if c not in nosalibids:
-    #         uplots.TimeSeriesPlots(bid=c,quan='Salinity')
-    #     uplots.VelocitySeries(c)
-    #     uplots.Batt_Sub(c)
-    #     uplots.TrackMaps(c)
+    nopresbids=['300534062158460','300534062158480']
+    nosalibids=['300534060051570','300534060251600','300234068519450']
+    curbuoys,deadbuoys,orderbuoys,newdead=BM.getBuoys()
+    for c in curbuoys:
+        # if c =='300534062158480':
+        uplots.TimeSeriesPlots(bid=c)  # Temperature plots are default
+        if c not in nopresbids:
+            uplots.TimeSeriesPlots(bid=c,quan='Pressure')
+        if c not in nosalibids:
+            uplots.TimeSeriesPlots(bid=c,quan='Salinity')
+        uplots.VelocitySeries(c)
+        uplots.Batt_Sub(c)
+        uplots.TrackMaps(c)
     uplots.OverviewMap()
         
 
@@ -71,8 +72,8 @@ def upupGo():
     upy.UploadToPSC()
         
 if __name__=='__main__':
-    upupData()
-    # upupPlots()
+    # upupData()
+    upupPlots()
     # upupGo()
 
 
